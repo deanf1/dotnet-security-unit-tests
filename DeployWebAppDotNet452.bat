d7acab31359911e9194366ce84bd0d2890a9b9ad
deanf1 <dnfleming12@gmail.com>
2017-06-19 14:03:39 -0400

    Improve batch file performance

--- DeployWebAppDotNet452.bat
+++ DeployWebAppDotNet452.bat
@@ -4,6 +4,6 @@ IF EXIST "%cd%\DotNetUnitTests\Web.config" (
 COPY "%cd%\DotNetUnitTests\webconfigs\Web.452.config" "%cd%\DotNetUnitTests"
 REN "%cd%\DotNetUnitTests\Web.452.config" "Web.config"
 
-msbuild.exe .\DotNetUnitTests.sln /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=LocalWeb
+msbuild.exe /m .\DotNetUnitTests.sln /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=LocalWeb
 START "" http://localhost:8080
-CALL "C:\Program Files\IIS Express\iisexpress" /path:"%cd%\DotNetUnitTests" /port:8080
+START /d "C:\Program Files\IIS Express\" iisexpress.exe /path:"%cd%\DotNetUnitTests" /port:8080
