22f537af18099cdc4557e15d95d8e2e7f66f5eac
deanf1 <dnfleming12@gmail.com>
2017-06-21 15:00:58 -0400

    Simplify batch files

--- DeployWebAppDotNet46.bat
+++ DeployWebAppDotNet46.bat
@@ -4,6 +4,6 @@ IF EXIST "%cd%\DotNetUnitTests\Web.config" (
 COPY "%cd%\DotNetUnitTests\webconfigs\Web.46.config" "%cd%\DotNetUnitTests"
 REN "%cd%\DotNetUnitTests\Web.46.config" "Web.config"
 
-msbuild.exe /m .\DotNetUnitTests.sln /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=LocalWeb
+msbuild /m .\DotNetUnitTests.sln /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=LocalWeb
 START "" http://localhost:8080
 START /d "C:\Program Files\IIS Express\" iisexpress.exe /path:"%cd%\DotNetUnitTests" /port:8080
