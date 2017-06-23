0a9cca6a3c3c6aefd56901ff2ceac360213f0278
deanf1 <dnfleming12@gmail.com>
2017-06-23 16:44:53 -0400

    Add constructors

--- DotNetUnitTests/Student.cs
+++ DotNetUnitTests/Student.cs
@@ -2,6 +2,19 @@
 {
     public class Student
     {
+        public Student()
+        {
+
+        }
+
+        public Student(string lastName, string firstName, string username, string password)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Username = username;
+            Password = password;
+        }
+
         public virtual int ID { get; set; }
         public virtual string LastName { get; set; }
         public virtual string FirstName { get; set; }
